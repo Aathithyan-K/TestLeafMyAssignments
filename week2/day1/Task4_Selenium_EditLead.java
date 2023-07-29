cc6e9b5d32a4ed352da9f9dc6cb2f0307f413530
Aathithyan K <140268822+Aathithyan-K@users.noreply.github.com>
2023-07-30 00:08:08 +0530

    Aathithyan Week2 Day 1 Tasks Completed.

---
+++ week2/day1/Task4_Selenium_EditLead.java
@@ -0,0 +1,44 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task4_Selenium_EditLead {
+
+	public static void main(String[] args) throws InterruptedException {
+		//Selenium Update Lead Task
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		
+		driver.findElement(By.id("username")).sendKeys("demoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		
+		driver.findElement(By.linkText("Leads")).click();
+		
+		driver.findElement(By.linkText("Create Lead")).click();
+		
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Aditya Corp");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Aathithyan");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("K");
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Aathithyan");
+		driver.findElement(By.id("createLeadForm_lastNameLocal")).sendKeys("K");
+		driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("QA Department");
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("I am a Software Development Engineer in Test Engineer");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("aathithyank82@gmail.com");
+		driver.findElement(By.name("submitButton")).click();
+		
+		Thread.sleep(5000);
+		driver.findElement(By.linkText("Edit")).click();
+		
+		driver.findElement(By.id("updateLeadForm_description")).clear();
+		driver.findElement(By.id("updateLeadForm_importantNote")).sendKeys("Important notes added");
+		driver.findElement(By.name("submitButton")).click();
+		
+		String pageTitle = driver.getTitle();
+		System.out.println("Page Title is : " + pageTitle);
+	}
+
+}
